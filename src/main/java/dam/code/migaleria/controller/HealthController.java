7c3892360f3b0203b6ca4cfe72b7c29b8e978246
Marcomartinalvarez458 <marcomartinalvarez14@gmail.com>
2026-05-11 14:52:52 +0200

    Fix CrossOrigin import in HealthController

--- src/main/java/dam/code/migaleria/controller/HealthController.java
+++ src/main/java/dam/code/migaleria/controller/HealthController.java
@@ -4,9 +4,7 @@ import dam.code.migaleria.repository.FavoritoRepository;
 import dam.code.migaleria.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
