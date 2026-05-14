a01e8cc77d68dac1fc3ec822d16460bf801def35
Marco Martín <marcomartinalvarez14@gmail.com>
2026-05-14 11:25:38 +0200

    Update FavoritoRepository.java

--- src/main/java/dam/code/migaleria/repository/FavoritoRepository.java
+++ src/main/java/dam/code/migaleria/repository/FavoritoRepository.java
@@ -1,6 +1,7 @@
 package dam.code.migaleria.repository;
 
 import dam.code.migaleria.models.Favorito;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public interface FavoritoRepository extends JpaRepository<Favorito, Integer> {
     List<Favorito> findByUsuarioId(int usuarioId);
     Optional<Favorito> findByUsuarioIdAndPeliculaId(int usuarioId, int peliculaId);
     boolean existsByUsuarioIdAndPeliculaId(int usuarioId, int peliculaId);
+    @Transactional
     void deleteByUsuarioIdAndPeliculaId(int usuarioId, int peliculaId);
 }
 
